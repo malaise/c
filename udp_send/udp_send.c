64594afd7a88f5630ce161dd3c331c02c9c9ea5c
Pascal MALAISE <malaise@magic.fr>
2007-02-06 17:32:10 +0000

    Support lan address.

--- udp_send/udp_send.c
+++ udp_send/udp_send.c
@@ -4,8 +4,8 @@
 #include "socket.h"
 
 
-#define error(msg) { \
-  fprintf(stderr, "ERROR : %s.\n", msg); \
+#define error(msg, arg) { \
+  fprintf(stderr, "ERROR : %s %s.\n", msg, arg); \
   fprintf(stderr, "Usage : udp_send <port_spec> <dest_spec>\n"); \
   fprintf(stderr, "   <port_spec> ::= <port_name> | <port_no>\n"); \
   fprintf(stderr, "   <dest_spec> ::= lan <lan_name> | host <host_name>\n"); \
@@ -14,12 +14,13 @@
 int main (int argc, char *argv[]) {
     boolean ping_lan;
     soc_port port_no;
+    soc_host host_no;
     soc_token soc = init_soc;
     int res;
     char buff [500];
 
     /* Parse command line arguments : host_name */
-    if (argc != 4) error("Syntax");
+    if (argc != 4) error("Syntax", "");
 
     /* Syntax is Ping <port> lan <lan_name> or Ping <port> host <host_name> */
     if (strcmp(argv[2], "host") == 0) {
@@ -27,37 +28,41 @@ int main (int argc, char *argv[]) {
     } else if (strcmp(argv[2], "lan") == 0) {
       ping_lan = true;
     } else {
-      error("Syntax");
+      error("Syntax", argv[2]);
     }
 
 
     /* Create socket */
     res = soc_open(&soc, udp_socket);
     if (res != SOC_OK) {
-        perror("soc_open");
-        error("Socket creation");
+        error("Socket creation", soc_error(res));
     }
 
     /* Set destination */
-    port_no = (soc_port) atoi(argv[1]);
-    if (port_no == 0) {
-        res = soc_set_dest_name_service(soc, argv[3], ping_lan, argv[1]);
+    if (soc_str2host (argv[3], &host_no) == SOC_OK) {
+      if (soc_str2port (argv[1], &port_no) == SOC_OK) {
+        res = soc_set_dest_host_port(soc, &host_no, port_no);
+      } else {
+        res = soc_set_dest_host_service (soc, &host_no, argv[1]);
+      }
     } else {
+      if (soc_str2port (argv[1], &port_no) == SOC_OK) {
         res = soc_set_dest_name_port(soc, argv[3], ping_lan, port_no);
+      } else {
+        res = soc_set_dest_name_service(soc, argv[3], ping_lan, argv[1]);
+      }
     }
 
     if (res != SOC_OK) {
-        perror("soc_set_dest_xxx");
-        error("Setting destination");
+        error("Setting destination", soc_error(res));
     }
 
     /* Send */
     strcpy (buff, "Ah que coucou!");
     res = soc_send (soc, buff, (soc_length)strlen(buff));
     if (res != SOC_OK) {
-        perror("soc_send");
-        error("Sending message");
+        error("Sending message", soc_error(res));
     }
-
     exit (0);
 }
+
