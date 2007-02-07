a8ac18b50b49ec39b00fb82c4e66c7878c98712a
Pascal MALAISE <malaise@magic.fr>
2007-02-07 17:57:28 +0000

    Change syntax. Read message from arguments or stdin.

--- udp_send/udp_send.c
+++ udp_send/udp_send.c
@@ -1,68 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+#include "get_line.h"
 #include "socket.h"
 
+static void error(const char *msg, const char *arg)
+  __attribute__ ((noreturn));
+static void error(const char *msg, const char *arg) {
+  fprintf(stderr, "ERROR : %s %s\n", msg, arg);
+  fprintf(stderr, "Usage : udp_send <lan_or_host> <dest>:<port> [ <message> ]\n");
+  fprintf(stderr, "   <lan_or_host> ::= lan | host\n");
+  fprintf(stderr, "   <port> ::= <port_name> | <port_no>\n");
+  fprintf(stderr, "   <dest> ::= <lan_name> | <host_name> | <ip_address>\n");
+  exit (1);
+}
 
-#define error(msg, arg) { \
-  fprintf(stderr, "ERROR : %s %s.\n", msg, arg); \
-  fprintf(stderr, "Usage : udp_send <port_spec> <dest_spec>\n"); \
-  fprintf(stderr, "   <port_spec> ::= <port_name> | <port_no>\n"); \
-  fprintf(stderr, "   <dest_spec> ::= lan <lan_name> | host <host_name>\n"); \
-  exit (1); }
+static char message[1024 *1024];
 
 int main (int argc, char *argv[]) {
-    boolean ping_lan;
-    soc_port port_no;
-    soc_host host_no;
-    soc_token soc = init_soc;
-    int res;
-    char buff [500];
+  boolean dest_lan;
+  char host_name[1024];
+  soc_host host_no;
+  char port_name[256];
+  soc_port port_no;
+  soc_token soc = init_soc;
+  char buffer[1024];
+  int i, res, len;
 
-    /* Parse command line arguments : host_name */
-    if (argc != 4) error("Syntax", "");
+  /* Parse command line arguments */
+  /* Syntax is udp_send lan/host <dest>:<port> [ <message> ] */
+  if (argc < 3) error("Invalid number of arguments", "");
 
-    /* Syntax is Ping <port> lan <lan_name> or Ping <port> host <host_name> */
-    if (strcmp(argv[2], "host") == 0) {
-      ping_lan = false;
-    } else if (strcmp(argv[2], "lan") == 0) {
-      ping_lan = true;
-    } else {
-      error("Syntax", argv[2]);
-    }
+  if (strcmp(argv[1], "host") == 0) {
+    dest_lan = false;
+  } else if (strcmp(argv[1], "lan") == 0) {
+    dest_lan = true;
+  } else {
+    error("Invalid argument", argv[1]);
+  }
 
-
-    /* Create socket */
-    res = soc_open(&soc, udp_socket);
-    if (res != SOC_OK) {
-        error("Socket creation", soc_error(res));
-    }
-
-    /* Set destination */
-    if (soc_str2host (argv[3], &host_no) == SOC_OK) {
-      if (soc_str2port (argv[1], &port_no) == SOC_OK) {
-        res = soc_set_dest_host_port(soc, &host_no, port_no);
-      } else {
-        res = soc_set_dest_host_service (soc, &host_no, argv[1]);
+  /* Locate ':' in dest:port */
+  strcpy (host_name, argv[2]);
+  res = -1;
+  for (i = 0; i < (int) strlen(host_name); i++) {
+    if (host_name[i] == ':') {
+      if (res != -1) {
+        error ("Invalid destination", host_name);
       }
+      res = i;
+    }
+  }
+  /* ':' must exist and not at beginning or end */
+  if ( (res == -1) || (res == 0) || (res == (int)strlen (host_name) - 1) ) {
+    error ("Invalid destination", host_name);
+  }
+  host_name[res] = '\0';
+  strcpy (port_name, &host_name[res+1]);
+
+  /* Create socket */
+  res = soc_open(&soc, udp_socket);
+  if (res != SOC_OK) {
+      error("Socket creation", soc_error(res));
+  }
+
+  /* Set destination */
+  if (soc_str2host (host_name, &host_no) == SOC_OK) {
+    if (soc_str2port (port_name, &port_no) == SOC_OK) {
+      res = soc_set_dest_host_port(soc, &host_no, port_no);
     } else {
-      if (soc_str2port (argv[1], &port_no) == SOC_OK) {
-        res = soc_set_dest_name_port(soc, argv[3], ping_lan, port_no);
-      } else {
-        res = soc_set_dest_name_service(soc, argv[3], ping_lan, argv[1]);
+      res = soc_set_dest_host_service (soc, &host_no, port_name);
+    }
+  } else {
+    if (soc_str2port (host_name, &port_no) == SOC_OK) {
+      res = soc_set_dest_name_port(soc, host_name, dest_lan, port_no);
+    } else {
+      res = soc_set_dest_name_service(soc, host_name, dest_lan, port_name);
+    }
+  }
+  if (res != SOC_OK) {
+      error("Setting destination", soc_error(res));
+  }
+
+  /* Build or read message */
+  message[0] = '\0';
+  if (argc > 3) {
+    /* Concatenate arguments separated by spaces */
+    for (i = 3; i < argc; i++) {
+      if (i != 3) {
+        strcat (message, " ");
       }
+      strcat (message, argv[i]);
     }
+    len = strlen(message);
+  } else {
+    /* Read from stdin */
+    len = 0;
+    for (;;) {
+      /* Read a buffer of data from stdin */
+      res = get_line (NULL, buffer, sizeof(buffer));
+      if (res == -1) {
+        /* End of input flow */
+        break;
+      }
+      /* Concatenate to message */
+      memmove (&message[len], buffer, res);
+      len += res;
+      message[len] = '\n';
+      len ++;
+    }
+  }
 
-    if (res != SOC_OK) {
-        error("Setting destination", soc_error(res));
-    }
-
-    /* Send */
-    strcpy (buff, "Ah que coucou!");
-    res = soc_send (soc, buff, (soc_length)strlen(buff));
-    if (res != SOC_OK) {
-        error("Sending message", soc_error(res));
-    }
-    exit (0);
+  /* Send */
+  res = soc_send (soc, message, (soc_length)len);
+  if (res != SOC_OK) {
+      error("Sending message", soc_error(res));
+  }
+  exit (0);
 }
 
