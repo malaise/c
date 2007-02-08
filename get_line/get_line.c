b1829792734c9cd1cce6f58a11ada6c3ebffc849
Pascal MALAISE <malaise@magic.fr>
2007-02-08 17:42:11 +0000

    Add get_text that returns unsigned and get the line feed.

--- get_line/get_line.c
+++ get_line/get_line.c
@@ -3,15 +3,54 @@
 #include "get_line.h"
 
 /* Reads at most len chars from FILE (stdin if FILE=NULL) */
+/*  or until EOL (return) is entered. */
+/* The out string is always terminated by "\n\0" */
+/* Returns the strlen of str if OK */
+/* It returns 0 if EOF or error */
+extern unsigned int get_text (FILE *file, char *str,
+                              const unsigned int len) {
+    unsigned int u;
+    char c;
+
+    errno = 0;
+
+    if (len <= 0) return (0);
+
+
+    /* Read line */
+    u = 0;
+    do {
+        if (u >= len - 1) {
+          /* End of buffer */
+          break;
+        }
+        /* Read char */
+        if (file == (FILE*)NULL) {
+            c = (char) getchar();
+        } else {
+            c = (char) fgetc(file);
+        }
+        if ((int)c == EOF) {
+            /* Error or end of file */
+            break;
+        } else {
+            str[u] = c;
+            u++;
+        }
+
+    } while (c != EOL);
+
+    str[u] = NUL;
+    return (u);
+}
+
+/* Reads at most len chars from FILE (stdin if FILE=NULL) */
 /*  or until Cr (return) is entered. */
 /* The out string is always NUL terminated (Cr removed) */
 /* Returns the strlen of str if OK */
 /* It returns -1 if EOF or error */
-int get_line (file, str, len) 
-    FILE *file;
-    char str[];
-    int len;
-{
+int get_line (FILE *file, char *str, const int len) {
+
     int i;
     char c;
 
@@ -29,7 +68,7 @@ int get_line (file, str, len)
           /* End of buffer */
           str[i] = NUL;
           break;
-        } 
+        }
         /* Read char */
         if (file == (FILE*)NULL) {
             c = (char) getchar();
@@ -48,7 +87,7 @@ int get_line (file, str, len)
 
     } while (str[i] != NUL);
 
-       
+
     return (i);
 
 }
--- get_line/get_line.h
+++ get_line/get_line.h
@@ -1,15 +1,9 @@
-#ifndef	__GET_LINE__
+#ifndef __GET_LINE__
 #define __GET_LINE__
 
 /* INCLUDE FILES */
 /*****************/
 #include <stdio.h>
-
-#ifdef __STDC__
-#define ARGS(x) x
-#else
-#define ARGS(x) ()
-#endif
 
 #define NUL '\0'
 #define EOL '\n'
@@ -19,9 +13,20 @@
 /* The out string is always NUL terminated */
 /* Returns the strlen of str if OK */
 /* It returns -1 if EOF or error */
-extern int get_line 
-       ARGS(( FILE *file,
-              char *str,
-              int len));
+extern int get_line (FILE *file, char *str, const int len);
 
-#endif	/* _GET_LINE */
+/* Reads at most len chars from FILE (stdin if FILE=NULL) */
+/*  or until EOL (return) is entered. */
+/* The out string is always NUL terminated */
+/* Returns the strlen of str */
+/* Reads characters up to a New_Line (that is appended) */
+/*  or up to the end of file. */
+/*  So, either the returned string ends with a New_Line and */
+/*   another get can be performed, */
+/*  Or the string does not end with New_Line (or is empty) and */
+/*   the end of file has been reached or an error has occured. */
+extern unsigned int get_text (FILE *file, char *str,
+                              const unsigned int len);
+
+#endif /* _GET_LINE */
+
