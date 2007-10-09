5b3585b6a1190a57b0a1fa6b82547494df615117
Pascal MALAISE <malaise@magic.fr>
2007-10-09 06:24:53 +0000

    Make compile with gcc 4.1.2

--- mallocer/lib.c
+++ mallocer/lib.c
@@ -1,8 +1,14 @@
 #include <pthread.h>
 
+pthread_t th;
+
+static void * the_thread (void * arg) {
+  return arg;
+}
+
 extern void dummy (void);
 
 extern void dummy (void) {
-  (void) pthread_create (NULL, NULL, NULL, NULL);
+  (void) pthread_create (&th, NULL, &the_thread, NULL);
 }
 
